6afa1dd06f38b5d71365805b70146f1ca9d04077
Tomas Macri <emacri@educa.madrid.org>
2021-10-21 10:20:20 +0200

    correccion con indexOf mal hechoooo

--- Programación/TP 1 - Strings/src/main/java/main/Ejercicio5.java
+++ Programación/TP 1 - Strings/src/main/java/main/Ejercicio5.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        Ejercicio5 ej = new Ejercicio5();
-//        ej.ejConIndex(sc);
+      /*  Scanner sc = new Scanner(System.in);
+        Ejercicio5 ej = new Ejercicio5();
+        ej.ejConIndex(sc);
+    */
     }
 
     public String ejercicio(Scanner sc) {
@@ -22,23 +23,29 @@ public class Ejercicio5 {
         }
         return "Sus iniciales son: " + iniciales;
     }
-   /* public String ejConIndex(Scanner sc){
+  /* public String ejConIndex(Scanner sc){
         char caracter = ' ';
         String resultado = "";
         System.out.println("Ingrese su nombre y apellidos");
         String cadena = sc.nextLine();
         System.out.println(cadena.trim().charAt(0));
-        for (int i = 0; i < cadena.length(); i++) {
-            i = cadena.indexOf(i, caracter);
+        boolean noHayCaracteres = false;
+        for (int i = 0; i < cadena.length() && !noHayCaracteres; i++) {
+            i = cadena.indexOf(caracter,i);
             if (i != -1) {
                 resultado += cadena.charAt(i + 1);
             }
+            else{
+                noHayCaracteres = true;
+            }
         }
         return resultado;
-    }
 
-
-    Posible solucion con indexOf
-
-     */
+   */
 }
+
+
+
+
+
+
