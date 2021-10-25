1f3584bf61331ae59eeac6150108e04e46026cac
Tomas Macri <emacri@educa.madrid.org>
2021-10-25 11:11:27 +0200

    Correcion de algunos errores sonarLint, ahorcado casi terminado

--- Programación/TP 3 - Ahorcado/src/main/java/main/Main.java
+++ Programación/TP 3 - Ahorcado/src/main/java/main/Main.java
@@ -20,7 +20,10 @@ public class Main {
         int intentosRestantes = 8;
 
         // Palabra generada (nombre de un pais (faker.country().name()) o de un nombre para probar que diferencie tilde)
-        String palabraGenerada = faker.name().firstName(); //"diferencia tildes: á é í ó ú"
+        String palabraGenerada = faker.name().firstName();
+
+        //metodo para que si la palabra tiene tilde y el usuario ingresa el caracter comun, lo tome de todas formas
+        String palabraGeneradaSinAcentos = claseMain.guardarPalabraSinAcentos(palabraGenerada);
 
         char caracterCodificar = '_';
         // codifico la palabra generada (Los caracteres que no son letras los dejo igual)
@@ -51,7 +54,7 @@ public class Main {
             // Una vez que ingresó algo válido, me fijo si se arriesgó o si es un caracter (si ingresó + de 1 caracter pero no los de la misma longitud de la palabra, no contará como arriesgarse
            if (arriesgarFrase){
                System.out.println("Te has arriesgado por: " + ingresoUsuario);
-               arriesgarSolucion(ingresoUsuario, palabraCodificada, palabraGenerada); // Sugerencia de intellij: no hace falta igualar esta funcion a palabraCodificada
+               claseMain.arriesgarSolucion(ingresoUsuario, palabraCodificada, palabraGeneradaSinAcentos); // Sugerencia de intellij: no hace falta igualar esta funcion a palabraCodificada
 
                // como despues de arriesgar no hay segunda oportunidad, si acierta asigno la igualo la palabra codificada a la generada pero en ambos casos devuelvo 0
                intentosRestantes = 0;
@@ -60,10 +63,10 @@ public class Main {
                 // No se arriesgó, asigno la letra ingresad
                 letraIngresada = ingresoUsuario.charAt(0);
                 //busco si la letra ingresada esta en la palabra
-                if (claseMain.letraIngresadaCorrecta(letraIngresada, palabraGenerada)) {
+                if (claseMain.letraIngresadaCorrecta(letraIngresada, palabraGeneradaSinAcentos)) {
                     // si está, reemplazo los _____ por el caracter
                     System.out.println("Letra correcta");
-                    claseMain.reemplazarCaracteresCorrectos(letraIngresada, palabraGenerada, palabraCodificada); // Sugerencia de intellij: no hace falta igualar esta funcion a palabraCodificada
+                    claseMain.reemplazarCaracteresCorrectos(letraIngresada, palabraGenerada, palabraCodificada, palabraGeneradaSinAcentos); // Sugerencia de intellij: no hace falta igualar esta funcion a palabraCodificada
                 } else {
                     // si no esta, pierde un intento y agrego
                     intentosRestantes--;
@@ -84,6 +87,34 @@ public class Main {
             System.out.println("La palabra era " + palabraGenerada);
             System.out.println("GANASTE!");
         }
+    }
+
+    private String guardarPalabraSinAcentos(String palabraGenerada) {
+        StringBuilder palabraGeneradaSinAcentos = new StringBuilder();
+        for (int i = 0; i < palabraGenerada.length() ; i++) {
+            switch (palabraGenerada.toLowerCase().charAt(i)) {
+                // reemplazo la palabra por el caracter sin acento
+                case 'á':
+                    palabraGeneradaSinAcentos.append('a');
+                    break;
+                case 'é':
+                    palabraGeneradaSinAcentos.append('e');
+                    break;
+                case 'í':
+                    palabraGeneradaSinAcentos.append('i');
+                    break;
+                case 'ó':
+                    palabraGeneradaSinAcentos.append('o');
+                    break;
+                case 'ú':
+                    palabraGeneradaSinAcentos.append('u');
+                    break;
+                default:
+                    palabraGeneradaSinAcentos.append(palabraGenerada.charAt(i));
+                    break;
+            }
+        }
+        return palabraGeneradaSinAcentos.toString();
     }
 
     private StringBuilder codificarPalabraGenerada(String palabraGenerada, char caracterCodificacion) {
@@ -113,33 +144,29 @@ public class Main {
         return valido;
     }
 
-    private static StringBuilder arriesgarSolucion(String ingresoUsuario, StringBuilder palabraCodificada, String palabraGenerada) {
-        if (ingresoUsuario.equalsIgnoreCase(palabraGenerada)){
+    private void arriesgarSolucion(String ingresoUsuario, StringBuilder palabraCodificada, String palabraGeneradaSinAcentos) {
+        if (ingresoUsuario.equalsIgnoreCase(palabraGeneradaSinAcentos)){
             palabraCodificada.delete(0, palabraCodificada.length());
             palabraCodificada.append(ingresoUsuario);
         }
-        return palabraCodificada;
     }
 
-    private boolean letraIngresadaCorrecta(char letraIngresada, String palabraGenerada) {
+    private boolean letraIngresadaCorrecta(char letraIngresada, String palabraGeneradaSinAcentos) {
         boolean esCorrecta = false;
-        for (int i = 0; i < palabraGenerada.length(); i++) {
-            if (Character.toLowerCase(letraIngresada) == palabraGenerada.toLowerCase().charAt(i)) {
+        for (int i = 0; i < palabraGeneradaSinAcentos.length(); i++) {
+            if (Character.toLowerCase(letraIngresada) == palabraGeneradaSinAcentos.toLowerCase().charAt(i)) {
                 esCorrecta = true;
-                i = palabraGenerada.length();
+                i = palabraGeneradaSinAcentos.length();
             }
         }
         return esCorrecta;
     }
 
-    private StringBuilder reemplazarCaracteresCorrectos(char letraIngresada, String palabraGenerada, StringBuilder palabraCodificada) {
+    private void reemplazarCaracteresCorrectos(char letraIngresada, String palabraGenerada, StringBuilder palabraCodificada, String palabraGeneradaSinAcentos) {
         for (int i = 0; i < palabraGenerada.length() ; i++) {
-            if (Character.toLowerCase(letraIngresada) == palabraGenerada.toLowerCase().charAt(i)){
+            if (Character.toLowerCase(letraIngresada) == palabraGeneradaSinAcentos.toLowerCase().charAt(i)){
                 palabraCodificada.replace(i,i+1, palabraGenerada.charAt(i)+"");
             }
         }
-        return palabraCodificada;
     }
-
-
 }
