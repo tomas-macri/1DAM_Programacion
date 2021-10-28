a64aa8791a5bf50d0e14eead151add28833ea8ef
Tomas Macri <emacri@educa.madrid.org>
2021-10-28 13:43:44 +0200

    cambios bien hechos fin entornos Intellij

--- ENTORNOS/Intellij/videoclub_Esther/src/main/java/gui/Main.java
+++ ENTORNOS/Intellij/videoclub_Esther/src/main/java/gui/Main.java
@@ -36,7 +36,6 @@ public class Main {
     public static final String POR_FAVOR_ELIJA_UNA_DE_LAS_OPCIONES_DISPONIBLES = "Por favor, elija una de las opciones disponibles";
     public static final String SOLO_TENEMOS_3_TIPOS_DE_PRODUCTOS_DIME_1_2_O_3_POR_FAVOR = "Solo tenemos 3 tipos de productos, dime 1, 2 o 3, por favor.";
     public static final String GRACIAS_POR_SU_VISITA = "\n GRACIAS POR SU VISITA";
-    public static final String DIGAME_SU_DNI = "Digame su DNI";
     public static final String SU_MULTA_HA_SIDO_PAGADA = "Su multa ha sido pagada";
     public static final String ESTE_USUARIO_NO_TIENE_NINGUNA_MULTA = "Este usuario no tiene ninguna multa";
     public static final String ESTE_USUARIO_NO_ESTA_REGISTRADO = "Este usuario no esta registrado";
@@ -60,6 +59,7 @@ public class Main {
         //menu
         int opcion = 0;
         boolean seguir = true;
+        MainMethods mm = new MainMethods();
         do {
             do {
                 System.out.println(STRING_MENU);
@@ -72,312 +72,50 @@ public class Main {
             switch (opcion) {
                 case 1:
                     //1. addSocio
-                    addSocio(f, sv);
+                    mm.addSocio(f, sv);
                     Member member;
                     String dni;
                     break;
                 case 2:
                     //2. borrarSocio
-                    borrarSocio(sc, sv);
+                    mm.borrarSocio(sc, sv);
                     break;
                 case 3:
                     //3. addProducto
-                    addProducto(f, sc, sv);
+                    mm.addProducto(f, sc, sv);
                     break;
                 case 4:
                     //4. AddStockProducto
-                    addStockProducto(sc, sv);
+                    mm.addStockProducto(sc, sv);
                     break;
                 case 5:
                     //5. Alquilar
                     //encontrar producto,
-                    alquilar(sc, sv);
+                    mm.alquilar(sc, sv);
                     break;
                 case 6:
                     //6. Devolver
-                    devolver(sc, sv);
+                    mm.devolver(sc, sv);
                     break;
                 case 7:
                     //7. pagarMulta
-                    pagarMulta(sc, sv);
+                    mm.pagarMulta(sc, sv);
                     break;
                 case 8:
-                    seguir = salir();
+                    seguir = mm.salir();
                     break;
             }
         } while (seguir);
     }
 
-    private static boolean salir() {
-        boolean seguir;
-        System.out.println(GRACIAS_POR_SU_VISITA);
-        seguir = false;
-        return seguir;
-    }
 
-    private static void pagarMulta(Scanner sc, ServiciosVideoclub sv) {
-        String dni;
-        Member member;
-        System.out.println(DIGAME_SU_DNI);
-        dni = sc.nextLine();
-        if (sv.getSocio(dni) != null) {
-            member = sv.getSocio(dni);
-            if (member.isSancionado()) {
-                System.out.println(SU_MULTA_HA_SIDO_PAGADA);
-                member.setSancionado(false);
-            } else {
-                System.out.println(ESTE_USUARIO_NO_TIENE_NINGUNA_MULTA);
-            }
-        } else {
-            System.out.println(ESTE_USUARIO_NO_ESTA_REGISTRADO);
-        }
-    }
 
-    private static void devolver(Scanner sc, ServiciosVideoclub sv) {
-        String dni;
-        System.out.println(DIGAME_SU_DNI_PARA_PODER_PROCEDER_A_LA_DEVOLUCION);
-        dni = sc.nextLine();
-        if (sv.getSocio(dni) != null) {
-            System.out.println(PARA_PROCEDER_A_LA_DEVOLUCION_NOS_GUSTARIA_SABER_LA_PUNTUACION_DE_0_A_5_QUE_DARIA_AL_PRODUCTO);
-            int puntuacion;
-            do {
-                puntuacion = sc.nextInt();
-                sc.nextLine();
-            } while (puntuacion < 0 || puntuacion > 5);
-            System.out.println(VOLVERIA_A_ALQUILARLO_1_SI_2_NO);
-            int respuesta;
-            do {
-                respuesta = sc.nextInt();
-                sc.nextLine();
-            } while (respuesta < 1 || respuesta > 2);
-            boolean realquilar;
-            if (respuesta == 1) {
-                realquilar = true;
-            } else {
-                realquilar = false;
-            }
-            Poll poll = new Poll(puntuacion, realquilar);
-            //Servicios -> mirarfecha para multa,sacarAlquilerSocio, acctualizar producto cantidadAlquilada, addEncuestaAProducto
-            if (sv.devolverProducto(dni, poll)) {
-                System.out.println(DEVOLUCION_REALIZADA);
-            } else {
-                System.out.println(NO_TIENE_NINGUN_ALQUILER_CON_NOSOTROS);
-            }
-            if (sv.getSocio(dni).isSancionado()) {
-                System.out.println(HA_SIDO_SANCIONADO_YA_QUE_HA_DEVUELTO_EL_PRODUCTO_CON_RETRASO);
-            }
-        } else {
-            System.out.println(NO_ESTA_REGISTRADO_NO_PUEDE_TENER_NINGUN_ALQUILER_CON_NOSOTROS);
-        }
-    }
 
-    private static void alquilar(Scanner sc, ServiciosVideoclub sv) {
-        int opcion;
-        String dni;
-        int indiceProducto;
-        opcion = menuProducto(sc);
-        Producto productoAAlquilar = null;
-        switch (opcion) {
-            case 1:
-                if(sv.getTodosVideoJuegos().size() > 0) {
-                    indiceProducto = elegirVideojuego(sv, sc);
-                    productoAAlquilar = sv.getTodosVideoJuegos().get(indiceProducto);
-                }else{
-                    System.out.println(NO_DISPONEMOS_DE_ARTICULOS_DE_ESTE_TIPO_DISCULPE_LAS_MOLESTIAS);
-                }
-                break;
-            case 2:
-                if(sv.getTodosDocumentales().size() > 0) {
-                    indiceProducto = elegirDocumental(sv, sc);
-                    productoAAlquilar = sv.getTodosDocumentales().get(indiceProducto);
-                }else{
-                    System.out.println(NO_DISPONEMOS_DE_ARTICULOS_DE_ESTE_TIPO_DISCULPE_LAS_MOLESTIAS);
-                }
-                break;
-            case 3:
-                if(sv.getTodasPeliculas().size() > 0) {
-                    indiceProducto = elegirPelicula(sv, sc);
-                    productoAAlquilar = sv.getTodasPeliculas().get(indiceProducto);
-                }else{
-                    System.out.println(NO_DISPONEMOS_DE_ARTICULOS_DE_ESTE_TIPO_DISCULPE_LAS_MOLESTIAS);
-                }
-                break;
-        }
-        //nif, comprobar: si member alquilo, si multa, si stock producto; sumar a cantidadAlquilada
-        if (productoAAlquilar != null) {
-            System.out.println(PARA_PROCEDER_AL_ALQUILER_DEL_PRODUCTO_NECESITO_SU_DNI);
-            dni = sc.nextLine();
-            String alquilado = sv.alquilarProducto(productoAAlquilar, dni);
-            System.out.println(alquilado);
-            if (alquilado.equals(PRODUCTO_ALQUILADO_CORRECTAMENTE_MUCHAS_GRACIAS)) {
-                if (opcion == 1) {
-                    System.out.println(RECUERDE_QUE_TIENE + Configuration.getDiasAlquilerVideojuego()
-                            + SEGUNDOS_PARA_DEVOLVERLO_SIN_SER_SANCIONADO);
-                } else {
-                    System.out.println(RECUERDE_QUE_TIENE + Configuration.getDiasAlquilerPeliculas()
-                            + SEGUNDOS_PARA_DEVOLVERLO_SIN_SER_SANCIONADO);
-                }
-            }
-        }
-    }
 
-    private static void addStockProducto(Scanner sc, ServiciosVideoclub sv) {
-        int opcion;
-        opcion = menuProducto(sc);
-        int indiceProducto;
-        int cantidadACambiar;
-        switch (opcion) {
-            case 1:
-                if (sv.getTodosVideoJuegos().size() > 0) {
-                    indiceProducto = elegirVideojuego(sv, sc);
-                    System.out.println(MODIFICAR_STOCK);
-                    cantidadACambiar = sc.nextInt();
-                    sc.nextLine();
-                    Producto producto = sv.getTodosVideoJuegos().get(indiceProducto);
-                    actualizarStock(cantidadACambiar, producto, sv);
-                } else {
-                    System.out.println(NO_EXISTEN_PRODUCTOS_DE_ESTE_TIPO_TOOOORPE);
-                }
-                break;
-            case 2:
-                if (sv.getTodosDocumentales().size() > 0) {
-                    indiceProducto = elegirDocumental(sv, sc);
-                    System.out.println(MODIFICAR_STOCK);
-                    cantidadACambiar = sc.nextInt();
-                    sc.nextLine();
-                    Producto producto = sv.getTodosDocumentales().get(indiceProducto);
-                    actualizarStock(cantidadACambiar, producto, sv);
-                } else {
-                    System.out.println(NO_EXISTEN_PRODUCTOS_DE_ESTE_TIPO_TOOOORPE);
-                }
-                break;
-            case 3:
-                if (sv.getTodasPeliculas().size() > 0) {
-                    indiceProducto = elegirPelicula(sv, sc);
-                    System.out.println(DIME_LA_CANTIDAD_A_MODIFICAR_EN_CASO_DE_SER_UNA_RETIRADA_DE_STOCK_POR_FAVOR_INDICALO_EN_NEGATIVO);
-                    cantidadACambiar = sc.nextInt();
-                    sc.nextLine();
-                    Producto producto = sv.getTodasPeliculas().get(indiceProducto);
-                    actualizarStock(cantidadACambiar, producto, sv);
-                } else {
-                    System.out.println(NO_EXISTEN_PRODUCTOS_DE_ESTE_TIPO_TOOOORPE);
-                }
-                break;
-        }
-    }
 
-    private static void addProducto(Faker f, Scanner sc, ServiciosVideoclub sv) {
-        int opcion;
-        opcion = menuProducto(sc);
-        switch (opcion) {
-            case 1:
-                Producto vj = new Videogame(f.harryPotter().book(), f.number().numberBetween(1, 10), f.color().name(), f.animal().name());
-                registrarProducto(vj, sv);
-                break;
-            case 2:
-                Producto docu = new Documentary(f.harryPotter().book(), f.number().numberBetween(1, 10), f.color().name(), MovieFormat.DVD, f.gameOfThrones().character(), "120min");
-                registrarProducto(docu, sv);
-                break;
-            case 3:
-                Producto peli = new Movie(f.harryPotter().book(), f.number().numberBetween(1, 10), f.color().name(), MovieFormat.DVD, f.gameOfThrones().character(), "120min");
-                registrarProducto(peli, sv);
-                break;
-        }
-    }
 
-    private static void borrarSocio(Scanner sc, ServiciosVideoclub sv) {
-        String dni;
-        System.out.println(PARA_ELIMINAR_UN_SOCIO_DIGAME_EL_DNI_DE_ESE_SOCIO_POR_FAVOR);
-        dni = sc.nextLine();
-        if (sv.borrarSocio(dni)) {
-            System.out.println(SE_HA_ENCONTRADO_EL_SOCIO_Y_SE_HA_ELIMINADO_DE_NUESTRO_REGISTRO);
-        } else {
-            System.out.println(ESTE_SOCIO_NO_SE_ENCUENTRA_EN_NUESTRO_SISTEMA);
-        }
-    }
 
-    private static void addSocio(Faker f, ServiciosVideoclub sv) {
-        String dni = f.phoneNumber().extension();
-        System.out.println("Dni: " + dni);
-        String nombre = f.gameOfThrones().character();
-        String direccion = f.gameOfThrones().city();
-        String tel = f.phoneNumber().toString();
-        int edad = f.number().numberBetween(1, 99);
-        Member member = new Member(dni, nombre, direccion, tel, edad);
-        if (sv.addSocio(member)) {
-            System.out.println(SOCIO_REGISTRADO);
-        } else {
-            System.out.println(ESTE_SOCIO_YA_SE_ENCUENTRA_EN_NUESTRA_BASE_DE_DATOS);
-        }
-    }
 
-    private static void actualizarStock(int cantidadACambiar, Producto producto, ServiciosVideoclub sv) {
-        if (cantidadACambiar < 0) {
-            if ((cantidadACambiar * (-1)) > producto.getCantidad()) {
-                System.out.println(NO_DISPONEMOS_DE_TANTAS_UNIDADES_DE_ESTE_PRODUCTO_REVISE_LOS_DATOS_Y_VUELVA_A_REALIZAR_LA_OPERACION);
-            } else {
-                sv.actualizarStockProducto(producto, cantidadACambiar);
-            }
-        } else {
-            sv.actualizarStockProducto(producto, cantidadACambiar);
-            System.out.println(STOCK_ACTUALIZADO);
-        }
-        System.out.println(LA_CANTIDAD_AHORA_ES  + producto.getCantidad());
-    }
 
-    private static void registrarProducto(Producto p, ServiciosVideoclub sv) {
-        if (sv.addProducto(p)) {
-            System.out.println(PRODUCTO_AÑADIDO_CORRECTAMENTE);
-        } else {
-            System.out.println(EL_PRODUCTO_NO_SE_HA_PODIDO_AÑADIR);
-        }
-    }
-
-    private static int elegirProducto(Scanner sc, int size) {
-        System.out.println(ELIGE_EL_PRODUCTO_DESEADO_DE_LA_LISTA);
-        int opcion = 0;
-        do {
-            opcion = sc.nextInt();
-            sc.nextLine();
-            if (opcion < 1 || opcion > size) {
-                System.out.println(POR_FAVOR_ELIJA_UNA_DE_LAS_OPCIONES_DISPONIBLES);
-            }
-        } while (opcion < 1 || opcion > size);
-        return opcion - 1;
-    }
-
-    private static int menuProducto(Scanner sc) {
-        int opcion;
-        do {
-            System.out.println(STRING_MENU);
-            opcion = sc.nextInt();
-            sc.nextLine();
-            if (opcion < 1 || opcion > 3) {
-                System.out.println(SOLO_TENEMOS_3_TIPOS_DE_PRODUCTOS_DIME_1_2_O_3_POR_FAVOR);
-            }
-        } while (opcion < 1 || opcion > 3);
-        return opcion;
-    }
-
-    private static int elegirVideojuego(ServiciosVideoclub sv, Scanner sc) {
-        for (Videogame vj : sv.getTodosVideoJuegos()) {
-            System.out.println((sv.getTodosVideoJuegos().indexOf(vj) + 1) + " " + vj.toString());
-        }
-        return elegirProducto(sc, sv.getTodosVideoJuegos().size());
-    }
-
-    private static int elegirDocumental(ServiciosVideoclub sv, Scanner sc) {
-        for (Documentary docu : sv.getTodosDocumentales()) {
-            System.out.println((sv.getTodosDocumentales().indexOf(docu) + 1) + " " + docu.toString());
-        }
-        return elegirProducto(sc, sv.getTodosDocumentales().size());
-    }
-
-    private static int elegirPelicula(ServiciosVideoclub sv, Scanner sc) {
-        for (Movie peli : sv.getTodasPeliculas()) {
-            System.out.println((sv.getTodasPeliculas().indexOf(peli) + 1) + " " + peli.toString());
-        }
-        return elegirProducto(sc, sv.getTodasPeliculas().size());
-    }
 
 }
