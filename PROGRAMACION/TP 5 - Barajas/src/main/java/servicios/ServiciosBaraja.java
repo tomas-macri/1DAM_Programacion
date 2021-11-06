09e759b32f2434f3b1c25338a5402488c27a4840
tomas <emacri@educa.madrid.org>
2021-11-06 21:14:23 +0100

    trabajo sabado zzz

--- PROGRAMACION/TP 5 - Barajas/src/main/java/servicios/ServiciosBaraja.java
+++ PROGRAMACION/TP 5 - Barajas/src/main/java/servicios/ServiciosBaraja.java
@@ -1,12 +1,35 @@
 package servicios;
 
+import java.util.Random;
+
 public class ServiciosBaraja {
     public static void main(String[] args) {
 
     }
 
-    public void inicializarBaraja(int cantCartas, int cartasPorPalo){
-        //inicializar
-        //mezclar
+    public int[] inicializarBaraja(int cantCartas, int cartasPorPalo){
+        int[] baraja = new int[cantCartas];
+        for (int i = 0; i < cantCartas; i++) {
+            baraja[i] = (i%cartasPorPalo)+1;
+        }
+        // tengo baraja ordenada
+        //barajar
+        barajar(baraja);
+        return baraja;
+    }
+
+    private int[] barajar(int[] baraja){
+        Random posi = new Random();
+        int posicionArray1;
+        int posicionArray2;
+        int cartaPosi1;
+        for (int i = 0; i < 1000; i++) {
+            posicionArray1 = posi.nextInt(baraja.length-1);
+            posicionArray2 = posi.nextInt(baraja.length-1);
+            cartaPosi1 = baraja[posicionArray1];
+            baraja[posicionArray1] = baraja[posicionArray2];
+            baraja[posicionArray2] = cartaPosi1;
+        }
+        return baraja;
     }
 }
--- PROGRAMACION/TP 5 - Barajas/src/main/java/ui/Main.java
+++ PROGRAMACION/TP 5 - Barajas/src/main/java/ui/Main.java
@@ -1,7 +1,45 @@
 package ui;
 
+import servicios.ServiciosBaraja;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        final int cantSubBaraja = 10;
+        Scanner sc = new Scanner(System.in);
+        char tipoBaraja;
+        int baraja[];
+        ServiciosBaraja serviciosBaraja = new ServiciosBaraja();
+        do {
+            System.out.println("Ingrese f para baraja francesa o e para española");
+            tipoBaraja = sc.nextLine().charAt(0);
+        }while (tipoBaraja != 'f' && tipoBaraja != 'e');
+        if (tipoBaraja == 'e'){
+            baraja = serviciosBaraja.inicializarBaraja(40, 10);
+        }
+        else{
+            baraja = serviciosBaraja.inicializarBaraja(52,13);
+        }
+
+        int subBaraja1[] = new int[cantSubBaraja];
+        for (int i = 0; i < subBaraja1.length; i++) {
+            subBaraja1[i] = baraja[i];
+        }
+
+        // Hago lo mismo pero agrego la cantidad de cartas repartidas para que no sean iguales
+        int subBaraja2[] = new int[cantSubBaraja];
+        for (int i = subBaraja1.length; i < subBaraja2.length + subBaraja1.length; i++) {
+            subBaraja2[i - subBaraja1.length] = baraja[i];
+        }
+
+        for (int i = 0; i < cantSubBaraja; i++) {
+            //comparar y sacar puntos
+            //if ()
+        }
+
+
+
 
     }
 //hacer proyecto zzzzzzz
