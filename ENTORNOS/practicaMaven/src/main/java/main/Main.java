467c5a4a79641d3501f582f3bc86835b8d98a492
tomas <emacri@educa.madrid.org>
2022-02-20 16:27:33 +0100

    fin medio maven

--- ENTORNOS/practicaMaven/src/main/java/main/Main.java
+++ ENTORNOS/practicaMaven/src/main/java/main/Main.java
@@ -1,7 +1,20 @@
 package main;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Main {
+    // el all args constructor esta para verificar que el lombok funciona correctamente
+
+    /*
+    *
+    *
+    *
+    * este es mi main
+    *
+    */
+
     public static void main(String[] args) {
-                            System.out.println("Este es el proyecto de maven");
+        System.out.println("Este es el proyecto de maven");
     }
 }
