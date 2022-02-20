c8bbb6a428543171a96056f05b4e629a3b86c8a5
tomas-macri <emacri@educa.madrid.org>
2022-02-21 00:10:32 +0100

    maven semi hecho

--- ENTORNOS/practicaMaven/src/main/java/main/Main.java
+++ ENTORNOS/practicaMaven/src/main/java/main/Main.java
@@ -7,12 +7,12 @@ public class Main {
     // el all args constructor esta para verificar que el lombok funciona correctamente
 
     /*
-    *
-    *
-    *
-    * este es mi main
-    *
-    */
+     *
+     *
+     *
+     * este es mi main
+     *
+     */
 
     public static void main(String[] args) {
         System.out.println("Este es el proyecto de maven");
