1073750a5f7ce4896a104e8334164187b0ec9089
tomas-macri <emacri@educa.madrid.org>
2022-03-15 14:11:34 +0100

    ficheros inicio

--- ENTORNOS/PRUEBAS/src/main/java/servicios/ServiciosCalculadora.java
+++ ENTORNOS/PRUEBAS/src/main/java/servicios/ServiciosCalculadora.java
@@ -10,26 +10,31 @@ public class ServiciosCalculadora {
         return op == '+' || op == '-' || op == '*' || op == '/';
     }
 
-    public int hacerOperacion(int num1, int num2, char op){
+    public int hacerOperacion(int num1, int num2, char op) {
         int resultado = -1;
-        switch (op){
-            case '+':
-                resultado = num1 + num2;
-                break;
-            case '-':
-                resultado = num1 - num2;
-                break;
-            case '*':
-                resultado = num1 * num2;
-                break;
-            case '/':
-                resultado = num1 / num2;
-                break;
-            default:
-                break;
+        if (numeroValido(num1) && numeroValido(num2) && operacionValida(op)) {
+            switch (op) {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    if (num2 < num1){
+                        resultado = num1 - num2;
+                    }
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
         return resultado;
     }
-
 
 }
--- ENTORNOS/PRUEBAS/src/main/java/ui/Main.java
+++ ENTORNOS/PRUEBAS/src/main/java/ui/Main.java
@@ -13,20 +13,20 @@ public class Main {
 
         int num1;
         int num2;
-        do {
+        //do {
             System.out.println("Ingrese un numero");
             num1 = sc.nextInt();
             sc.nextLine();
             System.out.println("Ingrese otro numero");
             num2 = sc.nextInt();
             sc.nextLine();
-        }while (!(serviciosCalculadora.numeroValido(num1) && serviciosCalculadora.numeroValido(num2)));
+        //}while (!(serviciosCalculadora.numeroValido(num1) && serviciosCalculadora.numeroValido(num2)));
 
         char operacion;
-        do {
+        //do {
             System.out.println("Ingrese la operacion a realizar (+ - * /): ");
             operacion = sc.nextLine().charAt(0);
-        }while (!serviciosCalculadora.operacionValida(operacion));
+        //}while (!serviciosCalculadora.operacionValida(operacion));
         System.out.println("El resultado es " + serviciosCalculadora.hacerOperacion(num1, num2, operacion));
     }
 }
